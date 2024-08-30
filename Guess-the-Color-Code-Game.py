2f91b416923e9d3d5b705627d0ba138fde207e82
SakshamSharma23 <141473239+SakshamSharma23@users.noreply.github.com>
2024-08-30 22:29:05 +0530

    Add files via upload

---
+++ Guess-the-Color-Code-Game.py
@@ -0,0 +1,78 @@
+import random
+
+COLORS=["R","G","B","Y","W","O"]
+TRIES = 10
+CODE_LENGTH=4
+def generate_code():
+    code=[]
+    for i in range(CODE_LENGTH):
+        color=random.choice(COLORS)
+        code.append(color)
+
+    return code
+
+def guess_code():
+    while True:
+        guess=input("Guess: ").upper().split(" ")
+
+        if len(guess)!=4:
+            print(f"code length must be equal to {CODE_LENGTH}")
+            continue
+
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color: {color} Try again ")
+                break
+        else:
+            break
+    return guess
+
+def check_code(guess,real_code):
+    color_counts={}
+    correct_pos=0
+    incorrect_pos=0
+
+    for color in real_code:
+        if color not in color_counts:
+            color_counts[color]=0
+        color_counts[color]+=1
+
+    for guess_color, real_color in zip(guess,real_code):
+        if guess_color == real_color:
+            correct_pos+=1
+            color_counts[guess_color] -= 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_counts and color_counts[guess_color] >0:
+            incorrect_pos +=1
+            color_counts[guess_color] -=1
+    return correct_pos, incorrect_pos
+
+def game():
+    print("Welcome to mastermind game")
+    print("The availabe colors are :", COLORS)
+    print("The maximum attempts:",TRIES)
+    code=generate_code()
+    for attempts in range(1,TRIES+1):
+        guess = guess_code()
+        correct_pos, incorrect_pos = check_code(guess,code)
+        if correct_pos == CODE_LENGTH:
+            print(f"You guessed the code correct in {attempts} attempts")
+            break
+
+        print(f"Correct positions:{correct_pos} | Incorrect positions: {incorrect_pos}")
+    else:
+        print("You ran out of tries , the code was: ",code)
+
+game()
+
+
+
+
+
+
+
+
+
+
+
